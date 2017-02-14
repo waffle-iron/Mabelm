15f3e6edf597aba4104d162ddd3a6a9e26572846
sourcreme <jeremy@sourcre.am>
2017-02-13 22:43:56 -0800

    adding icons

--- development/elm/src/View/View.elm
+++ development/elm/src/View/View.elm
@@ -2,11 +2,15 @@ module View.View exposing (..)
 
 import Html exposing (Html, text, div, ul, li, span)
 import Html.Attributes exposing (class, id)
-import Material.Icons.Action exposing (account_balance)
+import Material.Icons.Action exposing (build)
+import Color as Color
+import Svg exposing (Svg)
 
 
 import Messages exposing (Msg(..))
 import Model exposing (Model)
+
+type alias MaterialIcon = (Color.Color -> Int -> Svg Msg)
 
 view : Model -> Html Msg
 view model =
@@ -18,15 +22,15 @@ view model =
 toolbar : Model -> Html Msg
 toolbar model =
     div [ class "toolbar" ]
-        [ toolbar__button model
-        , toolbar__button model
-        , toolbar__button model
+        [ toolbar__button model build (Color.rgb 200 200 200) 50
+        , toolbar__button model build (Color.rgb 200 200 200) 50
+        , toolbar__button model build (Color.rgb 200 200 200) 50
         ]
 
-toolbar__button : Model -> Html Msg
-toolbar__button model =
+toolbar__button : Model -> MaterialIcon -> Color.Color -> Int -> Html Msg
+toolbar__button model icon color size =
     div [ class "toolbar__button" ]
-        [
+        [ icon color size
         ]
 
 
